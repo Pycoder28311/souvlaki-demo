65b5f052cd05bd526e75ef548e1e5287a7af51e1
Pycoder28311 <kopotitore@gmail.com>
2025-10-09 01:52:07 +0300

    Your commit message

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -375,7 +375,7 @@ export default function OrderSidebar({
 
       {/* Total and Checkout */}
       {orderItems.length > 0 && user?.email !== "kopotitore@gmail.com" && (
-        <div className="mt-4 border-t border-gray-400 pt-4 px-2 sm:px-0">
+        <div className="border-t border-gray-400 pt-4 px-2 sm:px-0">
           <button
             onClick={() => setShowPaymentModal(true)}
             className="w-full bg-yellow-400 text-gray-800 py-3 sm:py-2 text-lg sm:text-base rounded-xl font-semibold hover:bg-yellow-500 transition"
@@ -386,7 +386,7 @@ export default function OrderSidebar({
       )}
 
       {orderItems.length === 0 && user?.email !== "kopotitore@gmail.com" && (
-        <div className="mt-4 border-t border-gray-400 pt-4 px-2 sm:px-0">
+        <div className="border-t border-gray-400 pt-4 px-2 sm:px-0">
           <Link href="/menu" className="block w-full">
             <button
               className="w-full bg-yellow-400 text-gray-800 py-3 sm:py-2 text-lg sm:text-base font-bold rounded-lg hover:bg-yellow-500 transition"
